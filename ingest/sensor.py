e290dda3274cf4fc2eaab78c0f656d5603672f71
brianhouse <brian.house@gmail.com>
2015-04-09 00:00:56 -0400

    now with geoassigning

--- ingest/sensor.py
+++ ingest/sensor.py
@@ -1,24 +1,41 @@
-from housepy import config, log, strings, net
+from housepy import config, log, strings, net, util
 from ingest import ingest_plain_body
+from pymongo import ASCENDING, DESCENDING
+from housepy.server import Application
 
 def parse(request):
     log.info("sensor.parse")
     try:
         message = ingest_plain_body(request)
         data = net.urldecode(message)    
-        body = data['body']
-        log.debug(body)        
+        log.debug(data)
+        body = data['Body']
         content = strings.singlespace(body)
         tokens = content.split(" ")
-        log.debug(tokens)
-        data = {'t_utc': tokens[0], 'SensorName': tokens[1], strings.camelcase(tokens[2]): tokens[3]}
+
+        if tokens[0] == 'location':
+            lon, lat = tokens[3], tokens[2]   ## note reversal                        
+            # insert a record for this location update
+            data = {'t_utc': util.timestamp(), 'SensorName': tokens[1], 'longitude': lon, 'latitude': lat, 'LocationUpdate': True}
+
+        else:
+            # find the most recent location update for this sensor
+            db = Application.instance.db            
+            results = list(db.features.find({'properties.FeatureType': "sensor", 'properties.SensorName': tokens[1], 'properties.LocationUpdate': True}).sort([('properties.t_utc', DESCENDING)]).limit(1))
+            georef = results[0] if len(results) else None
+            geometry = georef['geometry'] if georef is not None else None
+            log.debug(geometry)
+            data = {'t_utc': tokens[0], 'SensorName': tokens[1], strings.camelcase(tokens[2]): tokens[3], 'LocationUpdate': False, 'geometry': geometry}
+
     except Exception as e:
         log.error("Twilio post is malformed: %s" % log.exc(e))
         return None
     return data
 
 """
-will we have location in this?
-if not, we'll want to prevent it from being estimated, right?
+if a sensor is placed without geometry, it will get picked up in the estimator
+
+assigning a location defines the geometry from then on out
+and any retroactive ones that are estimated
 
 """
