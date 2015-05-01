82b7aa7a7d81ce7d27a18cd47c849bd4602a2e2b
iaaaan <ian.ardouin@gmail.com>
2015-05-01 18:49:55 -0400

    fixed slides

--- static/js/vimeoCover.js
+++ static/js/vimeoCover.js
@@ -1,9 +1,7 @@
-console.log('1');
 var playerOrigin = '*';
 
 window.addEventListener('message', onMessageReceived, false);
 function onMessageReceived(event) {
-	console.log('2', event.origin);
     if (!(/^https?:\/\/player.vimeo.com/).test(event.origin)) {
         return false;
     }	        
@@ -11,13 +9,11 @@ function onMessageReceived(event) {
         playerOrigin = event.origin;
     }
     var data = JSON.parse(event.data);
-	console.log('2.5', data.event);
     if(data.event == 'ready') onReady();
     if(data.event == 'play') onPlay();
 }
 
 function onReady() {
-	console.log('3');
     var data = {
       method: 'addEventListener',
       value: 'play'
@@ -28,7 +24,6 @@ function onReady() {
 }
 
 function onPlay(){
-	console.log('4');
 	d3.select('#aboutPage #video div.cover')
 		.transition()
 		.style('opacity',0)
