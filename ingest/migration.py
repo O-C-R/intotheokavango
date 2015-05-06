70b98907a17d8f596476dae72303e1843ef743f1
brianhouse <brian.house@gmail.com>
2015-05-06 12:16:58 -0400

    differentiating between 13 and 14

--- ingest/migration.py
+++ ingest/migration.py
@@ -3,7 +3,7 @@ from ingest import ingest_json_body
 from housepy import config, log, strings, util
 
 def parse(request):
-    """Migrate okavango_14 data and fix tz issue with t_utcs"""
+    """Migrate okavango_13 and okavango_14 data and fix tz issue with t_utcs"""
     log.info("migration.parse")
     data = ingest_json_body(request)
     if 'properties' not in data:
@@ -15,11 +15,10 @@ def parse(request):
     if 't_utc' not in data['properties']:
         log.error("t_utc missing")
         return None
-    data['properties']['expedition'] = "okavango_14"
     if 'ContentType' in data['properties']:
         del data['properties']['ContentType']
 
-    """ hacks for correcting okavango_14 data go here """
+    """ hacks for correcting okavango_13 and okavango_14 data go here """
     
     # Bird Name should be SpeciesName    
     for key, item in data['properties'].items():
@@ -32,5 +31,11 @@ def parse(request):
     t_utc = strings.as_numeric(data['properties']['t_utc'])
     t_utc -= 180000
     data['properties']['t_utc'] = t_utc    
+
+    dt = datetime.datetime.utcfromtimestamp(t_utc)
+    if dt.year == 2013:
+        data['properties']['expedition'] = "okavango_14"    
+    elif dt.year == 2014:
+        data['properties']['expedition'] = "okavango_13"    
 
     return data
