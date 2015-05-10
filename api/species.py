b72521f6aebecadd734b2fdeb681aff18aabcd41
brianhouse <brian.house@gmail.com>
2015-05-10 19:45:27 -0400

    species counts

--- api/species.py
+++ api/species.py
@@ -17,4 +17,5 @@ def assemble(self, search, limit, order, resolution):
         return self.error(log.exc(e))
     species = results
     total = returned = len(species)        
+    del search['SpeciesName']
     return species, total, returned
