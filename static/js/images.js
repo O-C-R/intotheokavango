e550d9401a5c2da5457c0cd5ee9654db63522242
Genevieve Hoffman <genny.hoffman@gmail.com>
2015-05-15 12:34:52 -0400

    adding images to div dynamically

--- static/js/images.js
+++ static/js/images.js
@@ -10,15 +10,39 @@ function initImageLayout () {
 function loadData () {
     $.getJSON(path_to_data, function(data) {
 
-        var featureCollection = data['results'];
-        if(feature_type === "image" || feature_type === "sighting") {
+        var properties = data.results.features;
+        console.log("feature_type: " + feature_type);
+        console.log("path_to_data: " + path_to_data);
+        console.log(properties);
+
+        if(feature_type === "sighting") {
+
+            for (d in data.results.features) {
+                var item = data.results.features[d];
+                var images = item["properties"]["Images"];
+
+                for (i in images) {
+                    var image = images[i];
+                    var imageUrl = image.Url;
+                    console.log("image Url: " + imageUrl);
+                    $("#images").append("<image class='image' src='" + imageUrl + "'/>");
+                }
+            }
             //layout images
+
+        } else if (feature_type === "image") {
+
+            for (d in data.results.features) {
+                var item = data.results.features[d];
+                var imageUrl = item["properties"]["Url"];
+                console.log("image Url: " + imageUrl);
+                $("#images").append("<image class='image' src='" + imageUrl + "'/>");
+            }
+
         } else {
             console.log("no images for this feature");
         }
-        console.log("feature_type: " + feature_type);
-        console.log("path_to_data: " + path_to_data);
-        console.log(featureCollection);
+
     }).error(function(e) { console.log("Failed to load " + path_to_data + ": " + e.statusText); });    
 }
 
