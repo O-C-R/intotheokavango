805abc2b1befe369604bc571326af1a1c915d6eb
Genevieve Hoffman <genny.hoffman@gmail.com>
2015-05-15 11:55:41 -0400

    testing

--- static/js/images.js
+++ static/js/images.js
@@ -9,7 +9,13 @@ function initImageLayout () {
 /* load data file */
 function loadData () {
     $.getJSON(path_to_data, function(data) {
+
         var featureCollection = data['results'];
+        if(feature_type === "image" || feature_type === "sighting") {
+            //layout images
+        } else {
+            console.log("no images for this feature");
+        }
         console.log("feature_type: " + feature_type);
         console.log("path_to_data: " + path_to_data);
         console.log(featureCollection);
