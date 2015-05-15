36bcedd6ad8f7ed5247f41aeabf47039292ddec0
Genevieve Hoffman <genny.hoffman@gmail.com>
2015-05-15 11:34:01 -0400

    building out image view, for dev server testing

---
+++ static/js/images.js
@@ -0,0 +1,23 @@
+/* global variables */
+var imageLayout;                    
+
+/* create the layout */
+function initImageLayout () {
+    console.log("laying out images");
+}
+
+/* load data file */
+function loadData () {
+    $.getJSON(path_to_data, function(data) {
+        var featureCollection = data['results'];
+        console.log(featureCollection);
+    }).error(function(e) { console.log("Failed to load " + path_to_data + ": " + e.statusText); });    
+}
+
+
+/* executes on load */
+$(document).ready(function() {
+    loadData();
+    initImageLayout();
+});
+
