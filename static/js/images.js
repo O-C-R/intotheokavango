1b2c6f818ef9076c4f4b5c27835cf3d9934004f8
Genevieve Hoffman <genny.hoffman@gmail.com>
2015-05-15 11:44:38 -0400

    console feature_type

--- static/js/images.js
+++ static/js/images.js
@@ -10,6 +10,8 @@ function initImageLayout () {
 function loadData () {
     $.getJSON(path_to_data, function(data) {
         var featureCollection = data['results'];
+        console.log("feature_type: " + feature_type);
+        console.log("path_to_data: " + path_to_data);
         console.log(featureCollection);
     }).error(function(e) { console.log("Failed to load " + path_to_data + ": " + e.statusText); });    
 }
