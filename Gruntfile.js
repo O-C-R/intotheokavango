7c177f0cff71b04eeccec91a554b7c126088955c
iaaaan <ian.ardouin@gmail.com>
2015-05-18 18:19:38 -0400

    set Grunt up in order to concat all JS files

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: ['static/js/dummy_data.js','static/js/viz.js','static/js/dataPage.js','static/js/feed.js','static/js/features.js','static/js/layout.js','static/js/loader.js','static/js/members.js','static/js/timeline.js','static/js/wanderer.js','static/js/utils.js','static/js/main.js'],
+        dest: 'static/js/build.js',
+      }
+    },
+    shell: {
+        run: {
+            command: 'sudo ./main.py 7777'
+        }
+    }
+
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-shell');
+
+  // Default task(s).
+  grunt.registerTask('default', ['concat','shell:run']);
+
+};
