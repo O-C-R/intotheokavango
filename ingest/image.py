3bb27605ffd9df098f553c2c51b58b0f34092dd0
brianhouse <brian.house@gmail.com>
2015-05-22 16:41:12 -0400

    now with sightid links

--- ingest/image.py
+++ ingest/image.py
@@ -1,6 +1,8 @@
 import json
 from ingest import ingest_json_body, save_files, process_image, ingest_data
 from housepy import config, log, util, strings
+from housepy.server import Application
+from bson.objectid import ObjectId
 
 """Expecting JSON or form metadata with Member and a timestamp in the local timezone"""
 
@@ -28,9 +30,14 @@ def parse(request):
     # fix things
     if 'TeamMember' in data:
         data['Member'] = data['TeamMember']
-        del data['TeamMember']            
+        del data['TeamMember']   
+    if 'SightingID' in data and not len(data['SightingID'].strip()):
+        data['SightingID'] = None
+    if 'Notes' in data and not len(data['Notes'].strip()):
+        data['Notes'] = None
 
     # process the image -- add the same metadata for each one, and update with the image's data
+    images = []
     for path in paths:        
         if path[-4:] != "json":
             image_data = process_image(path, data['Member'] if 'Member' in data else None, data['t_utc'] if 't_utc' in data else None)
@@ -40,6 +47,29 @@ def parse(request):
             if not success:
                 return None, value
             ## note that this might still end up ingesting prior images
+
+            if 'Member' in image_data:
+                del image_data['Member']
+            images.append(image_data)
+
+    if 'SightingID' in data and data['SightingID'] is not None:
+        log.info("Linking to sighting %s..." % data['SightingID'])
+        db = Application.instance.db            
+        try:
+            feature = db.features.find_one({'_id': ObjectId(data['SightingID'])})
+            if feature is None:
+                log.warning("--> sighting with id %s not found..." % data['SightingID'])
+                ## no feedback given here
+            else:
+                current_images = feature['properties']['Images']
+                log.debug("current_images %s" % current_images)
+                images += current_images
+                log.debug("images %s" % images)
+                result = db.features.update({'_id': ObjectId(data['SightingID'])}, {'$set': {'properties.Images': images}})
+                log.debug(result)
+                log.info("--> success")
+        except Exception as e:
+            log.error(e)
 
     # stop executing, but still return success
     return True     
