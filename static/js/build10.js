e368025ec22b7dee292692ed99a8e87c03c5a951
Genevieve Hoffman <genny.hoffman@gmail.com>
2015-06-23 11:21:17 -0400

    build files

--- static/js/build10.js
+++ static/js/build10.js
@@ -772,6 +772,29 @@ var d3Graph = function(timelineVizID, totalsVizID){
         })
     }
 
+    var speciesCountObj = {};
+    var speciesCountArray = [];
+
+    var getSpeciesCount = function(sightings) {
+
+            for (var i = 0; i < sightings.length; ++i) {
+                if (!speciesCountObj.hasOwnProperty(sightings[i].properties.SpeciesName)) {
+                    speciesCountObj[sightings[i].properties.SpeciesName] = 0;
+                    console.log("doesn't have species property");
+                }
+                speciesCountObj[sightings[i].properties.SpeciesName] += sightings[i].properties.Count;
+            }
+            console.log(Object.keys(speciesCountObj));
+
+            for (s in speciesCountObj){
+                myObj = {};
+                myObj.species = s;
+                myObj.count  = speciesCountObj[s];
+                speciesCountArray.push(myObj);
+            }
+            console.log(speciesCountArray);
+        }
+
     //get the totals for all features, recursively - then draw a totals bar chart viz
     var getFeatureTotalData = function(featureType) {
         var url = "http://intotheokavango.org/api/features?FeatureType=" + featureType + "";
@@ -827,6 +850,7 @@ var d3Graph = function(timelineVizID, totalsVizID){
                 return error;
             } else {
                 console.log("Initial Data", data);
+                console.log("feature_type", feature_type);
 
                 //parse item differently based on feature_type
                 if (feature_type === "None" && path_to_data.indexOf("features") != -1 ) { //top level features
@@ -943,6 +967,25 @@ var d3Graph = function(timelineVizID, totalsVizID){
                         //timelineVizDiv.fadeIn();
                         //totalsVizDiv.hide();
                         makeHistogramPlot(parsedSpeciesSighting, feature_type, speciesName);
+                    } else {
+
+                        getSpeciesCount(data.results.features);
+                        makeTotalsViz(speciesCountArray);
+                        // console.log("these are the species");
+                        // for(species in data.results) {
+                            
+                        //     var count = data.results[species];
+                        //     console.log(species + ": " + count);
+
+                        //     var sightingCount = {};
+                        //     sightingCount.type = species;
+                        //     sightingCount.total = count;
+                        //     speciesSightingsTotals.push(sightingCount);
+                        // }
+                        // console.log(speciesSightingsTotals);
+                        // //totalsVizDiv.fadeIn();
+                        // makeTotalsViz(speciesSightingsTotals);
+                        console.log("no species in query");
                     }
                 }
                 if (feature_type === "sensor") {
@@ -3271,19 +3314,13 @@ function Loader(){
 
 		// http://intotheokavango.org/api/features?FeatureType=sighting&Expedition=okavango_15&expeditionDay=7&limit=0
 		var query = 'http://intotheokavango.org/api/features?FeatureType=sighting&Expedition=okavango_'+expeditionYear+'&expeditionDay='+(day+timeOffsets[expeditionYear].query)+'&limit=0'
-		console.log(query);
 		d3.json(query, function(error, data) {
-
 			if(error) return console.log("Failed to load " + query + ": " + error.statusText);
 			data = data.results;	
-
-			console.log(data.features);
-
+			
 		    L.geoJson(data.features, {
 		        filter: function(feature, layer) {
-		        	console.log('aga', feature);
-		        	// if(!feature.properties.CoreExpedition) return false;
-		            console.log(feature);
+		        	if(!feature.properties.CoreExpedition) return false;
 		        	if(feature.geometry == 'null') return false;
 		            return (feature.geometry.coordinates[0] != 0);
 		        },
@@ -3475,6 +3512,86 @@ function Loader(){
 		expeditionYear: expeditionYear	
 	};
 }
+
+;/* global variables */
+var map;                     // the map object
+
+/* create the map */
+function initMap () {
+    map = new L.map('map', {
+        layers: new L.TileLayer("http://a.tiles.mapbox.com/v3/" + mapbox_username + ".map-" + mapbox_map_id + "/{z}/{x}/{y}.png"),
+        center: new L.LatLng(-19.003049, 22.414856), //was -17.003049, 20.414856; Menongue is -14.645009, 17.674752
+        zoomControl: true,
+        attributionControl: false,
+        doubleClickZoom: true,
+        scrollWheelZoom: true,
+        boxZoom: true,
+        touchZoom: false,
+        dragging: true,
+        keyboard: true,
+        zoom: 7,
+        minZoom: 1,                    
+        maxZoom: 20
+    });    
+
+    zoomLevel = map.getZoom();
+    //console.log("zoom level: " + zoomLevel); 
+}
+
+var geojsonMarkerOptions = {
+    radius: 5,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 2,
+    opacity: 1,
+    fillOpacity: 0.8
+};
+
+/* load data file */
+function loadData () {
+    console.log("loadData");
+    console.log(path_to_data);
+    var url = "http://intotheokavango.org" + path_to_data;
+    console.log(url);
+    $.getJSON(url, function(data) {
+        var featureCollection = data['results'];
+        console.log(featureCollection);
+        var sightingsWithGeoLoc = [];
+        
+        for (d in featureCollection.features) {
+            var item = featureCollection.features[d];
+            if(item.geometry === null) {
+                //console.log("sighting has no geometry");
+            } else {
+                //console.log("sighting with geometry");
+                sightingsWithGeoLoc.push(item);
+            }
+        }
+        console.log(sightingsWithGeoLoc);
+        filteredFeatureCollection = {};
+        filteredFeatureCollection.features = sightingsWithGeoLoc;
+        filteredFeatureCollection.type = "FeatureCollection";
+
+        L.geoJson(filteredFeatureCollection, {
+            pointToLayer: function (feature, latlng) {
+                console.log("latlng: " + latlng);
+                console.log(feature['properties']['t_created']);
+                return L.circleMarker(latlng, geojsonMarkerOptions);
+            },
+            onEachFeature: function (feature, layer) {
+                layer.bindPopup("<span style=\"color: black;\">" + feature['properties']['FeatureType'] + "<br />" + feature['properties']['DateTime'] + "<br />" + feature['properties']['t_utc'] + "</span>");
+            }
+        }).addTo(map);
+    }).error(function(e) { console.log("Failed to load " + path_to_data + ": " + e.statusText); });    
+}
+
+
+/* executes on load */
+$(document).ready(function() {
+    console.log("LOADING MAP");
+    loadData();
+    initMap();
+});
 
 ;
 
