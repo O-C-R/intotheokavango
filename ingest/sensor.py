45fe87904272db7f43d76d1344620d1914e664bb
brianhouse <brian.house@gmail.com>
2015-07-09 17:22:10 -0400

    rebuilt sensor endpoint

--- ingest/sensor.py
+++ ingest/sensor.py
@@ -25,28 +25,40 @@ def parse(request):
         number = data['From']
         body = data['Body']
         content = strings.singlespace(body.strip())
-        tokens = content.split(" ")
 
-        if tokens[0] == 'location':
-            lon, lat = tokens[3], tokens[2]   ## note reversal                        
-            # insert a record for this location update
-            data = {'t_utc': util.timestamp(), 'SensorName': tokens[1], 'longitude': lon, 'latitude': lat, 'LocationUpdate': True}
+        # 1436477098,Lat,-18.111946,Long,21.665733,TDS,4.000,Sal,0.000,WatTemp,16.88,AirTemp,18.50,Hum,48.90
+        tokens = content.split(',')
+        t = util.delocalize_timestamp(tokens[0], config['local_tz'])        
+        data = {'t_utc': t, 'FeatureType': "sensor"}
+        tokens = tokens[1:]
+        keys = tokens[0::2]
+        values = tokens[1::2]
+        for k, key in enumerate(keys):
+            data[strings.camelcase(key)] = strings.as_numeric(values[k])
 
-            # send a verification
-            send_sms(number, tokens[1], lon, lat)
 
-        else:
-            # find the most recent location update for this sensor
-            db = Application.instance.db            
-            results = list(db.features.find({'properties.FeatureType': "sensor", 'properties.SensorName': tokens[1], 'properties.LocationUpdate': True}).sort([('properties.t_utc', DESCENDING)]).limit(1))
-            georef = results[0] if len(results) else None
-            geometry = georef['geometry'] if georef is not None else None
-            log.debug(geometry)
-            if tokens[0] == "*":
-                t = util.timestamp()
-            else:                    
-                t = util.delocalize_timestamp(tokens[0], config['local_tz']) # converts from local time timestamp to t_utc
-            data = {'t_utc': t, 'SensorName': tokens[1], strings.camelcase(tokens[2]): tokens[3], 'LocationUpdate': False, 'geometry': geometry}
+        # tokens = content.split(" ")
+
+        # if tokens[0] == 'location':
+        #     lon, lat = tokens[3], tokens[2]   ## note reversal                        
+        #     # insert a record for this location update
+        #     data = {'t_utc': util.timestamp(), 'SensorName': tokens[1], 'longitude': lon, 'latitude': lat, 'LocationUpdate': True}
+
+        #     # send a verification
+        #     send_sms(number, tokens[1], lon, lat)
+
+        # else:
+        #     # find the most recent location update for this sensor
+        #     db = Application.instance.db            
+        #     results = list(db.features.find({'properties.FeatureType': "sensor", 'properties.SensorName': tokens[1], 'properties.LocationUpdate': True}).sort([('properties.t_utc', DESCENDING)]).limit(1))
+        #     georef = results[0] if len(results) else None
+        #     geometry = georef['geometry'] if georef is not None else None
+        #     log.debug(geometry)
+        #     if tokens[0] == "*":
+        #         t = util.timestamp()
+        #     else:                    
+        #         t = util.delocalize_timestamp(tokens[0], config['local_tz']) # converts from local time timestamp to t_utc
+        #     data = {'t_utc': t, 'SensorName': tokens[1], strings.camelcase(tokens[2]): tokens[3], 'LocationUpdate': False, 'geometry': geometry}
 
     except Exception as e:
         log.error("Twilio post is malformed: %s" % log.exc(e))
