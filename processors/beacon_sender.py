5224b6872e312afe6efb9fb96a290bcea645ef70
brianhouse <brian.house@gmail.com>
2015-07-14 12:37:54 -0400

    adding beacon emails

--- processors/beacon_sender.py
+++ processors/beacon_sender.py
@@ -13,7 +13,7 @@ def main():
         last_beacon = list(db.features.find({'properties.FeatureType': "beacon", 'properties.Satellite': {'$eq': core_sat}}).sort('properties.t_utc', -1).limit(1))[0]
         datetime = last_beacon['properties']['DateTime']
         lon, lat = last_beacon['geometry']['coordinates']
-        text = "Time: %s\nLat,Lon: %f,%f" % (datetime, lat, lon)
+        text = "Time:\n%s\n\nLat,Lon:\n%f,%f" % (datetime, lat, lon)
         log.info("--> last reported beacon (%s) at: %f,%f" % (datetime, lat, lon))
     except Exception as e:
         log.error("Could not get update: %s" % log.exc(e))
