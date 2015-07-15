f03e360100a9334164a2e31ea9c6992d4133f60e
brianhouse <brian.house@gmail.com>
2015-07-15 10:43:18 -0400

    adding my email

--- processors/beacon_sender.py
+++ processors/beacon_sender.py
@@ -4,7 +4,7 @@ import geojson, csv, dateutil, datetime, time, os, zipfile, pytz, xmltodict, jso
 from housepy import config, log, util, strings, emailer, net
 from mongo import db
 
-EMAILS = "300234011091180@ast.globalalerting.com"
+EMAILS = "300234011091180@ast.globalalerting.com,brian.house@gmail.com"
 
 def main():
     log.info("beacon_sender...")
