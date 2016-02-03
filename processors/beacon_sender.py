bfe2e3abdf2803778ad894dce09c033d4b495e19
brianhouse <brian.house@gmail.com>
2016-02-02 23:01:43 -0500

    updating sat emailer

--- processors/beacon_sender.py
+++ processors/beacon_sender.py
@@ -11,19 +11,20 @@ def main():
     if config['geo_emails'] is None or not len(config['geo_emails']):
         log.info("--> no emails")
         return
+    text = []
     try:
-        # core_sat = config['satellites'][0]
-        # last_beacon = list(db.features.find({'properties.FeatureType': "beacon", 'properties.Satellite': {'$eq': core_sat}}).sort('properties.t_utc', -1).limit(1))[0]
-        last_beacon = list(db.features.find({'properties.FeatureType': "beacon"}).sort('properties.t_utc', -1).limit(1))[0]        
-        datetime = last_beacon['properties']['DateTime']
-        lon, lat = last_beacon['geometry']['coordinates']
-        satellite = last_beacon['properties']['Satellite']
-        text = "Satellite:\n%s\n\nTime:\n%s\n\nLat,Lon:\n%f,%f" % (satellite, datetime, lat, lon)
-        log.info("--> last reported beacon (%s on %s) at: %f,%f" % (satellite, datetime, lat, lon))
+        for satellite in config['satellites']:
+            last_beacon = list(db.features.find({'properties.FeatureType': "beacon", 'properties.Satellite': {'$eq': satellite}}).sort('properties.t_utc', -1).limit(1))[0]
+            datetime = last_beacon['properties']['DateTime']
+            lon, lat = last_beacon['geometry']['coordinates']
+            satellite = last_beacon['properties']['Satellite']
+            text.append("%s\n%s\n%f,%f" % (satellite, datetime, lat, lon))
+            log.info("--> last reported beacon (%s on %s) at: %f,%f" % (satellite, datetime, lat, lon))
     except Exception as e:
         log.error("Could not get update: %s" % log.exc(e))
     try:
         log.info("Emailing to %s..." % EMAILS)
+        text = "\n\n".join(text)
         emailer.send(EMAILS, "Core beacon location", text)
     except Exception as e:
         log.error("Could not email: %s" % log.exc(e))
