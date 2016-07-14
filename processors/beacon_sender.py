f7bc2dfa0277c6c7bcb32248a4066f3f9102043c
brianhouse <brian.house@gmail.com>
2016-07-14 16:37:20 -0400

    beacon updates

--- processors/beacon_sender.py
+++ processors/beacon_sender.py
@@ -14,16 +14,21 @@ def main():
     text = []
     for satellite in config['satellites']:
         try:
-            last_beacon = list(db.features.find({'properties.FeatureType': "beacon", 'properties.Satellite': {'$eq': satellite}}).sort('properties.t_utc', -1).limit(1))[0]
-            datetime = last_beacon['properties']['DateTime']
+            last_beacon = list(db.features.find({'properties.FeatureType': "beacon", 'properties.Satellite': {'$eq': satellite}}).sort('properties.t_utc', -1).limit(1))
+            if not len(last_beacon):
+                continue
+            last_beacon = last_beacon[0]
+            dt = last_beacon['properties']['DateTime']
+            if (datetime.datetime.now(pytz.timezone(config['local_tz'])) - util.parse_date(dt)).days > 30:
+                continue
             lon, lat = last_beacon['geometry']['coordinates'][0], last_beacon['geometry']['coordinates'][1]
             satellite = last_beacon['properties']['Satellite']
             team = list(db.satellites.find({'Name': satellite}).sort('t_utc', -1).limit(1))[0]['Team']
-            if team is None:
-                continue
+            # if team is None:
+            #     continue
             google = "https://www.google.com/maps/place/%s,%s" % (lat, lon)
-            text.append("%s: %s\n%s\n%f,%f\n%s" % (satellite, team, datetime, lat, lon, google))
-            log.info("--> last reported beacon (%s: \"%s\" on %s) at: %f,%f" % (satellite, team, datetime, lat, lon))
+            text.append("%s: %s\n%s\n%f,%f\n%s" % (satellite, team, dt, lat, lon, google))
+            log.info("--> last reported beacon (%s: \"%s\" on %s) at: %f,%f" % (satellite, team, dt, lat, lon))
         except Exception as e:
             log.error("Could not get update: %s" % log.exc(e))
     try:
