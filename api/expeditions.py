efba9797c6826addd616b0fb3302416a11ea142a
brianhouse <brian.house@gmail.com>
2016-08-04 11:44:51 -0400

    pegging expedition endpoint to beacons for better accuracy

--- api/expeditions.py
+++ api/expeditions.py
@@ -9,7 +9,7 @@ def assemble(self, search, limit, order, resolution):
         results = self.db.features.find(search).distinct('properties.Expedition')
         for expedition in results:
             start_date = util.parse_date(str(config['start_date'][expedition]))
-            last_feature = list(self.db.features.find({'properties.Expedition': expedition}).sort([('properties.t_utc', DESCENDING)]).limit(1))[0]
+            last_feature = list(self.db.features.find({'properties.Expedition': expedition, 'properties.t_utc': {'$gt': util.timestamp(start_date)}, 'properties.FeatureType': "beacon"}).sort([('properties.t_utc', DESCENDING)]).limit(1))[0]
             end_date = util.parse_date(last_feature['properties']['DateTime'])
             duration = end_date - start_date
             expeditions[expedition] = {'StartDate': start_date, 'Days': duration.days}
