06a2a567d887ae5be8cb448bf78b9bd0001d33b8
brianhouse <brian.house@gmail.com>
2016-08-04 13:35:19 -0400

    improved expedition endpoint

--- api/expeditions.py
+++ api/expeditions.py
@@ -1,5 +1,5 @@
 import geojson
-from housepy import config, log, util
+from housepy import config, log, util, strings
 from pymongo import ASCENDING, DESCENDING
 
 def assemble(self, search, limit, order, resolution):
@@ -12,7 +12,7 @@ def assemble(self, search, limit, order, resolution):
             last_feature = list(self.db.features.find({'properties.Expedition': expedition, 'properties.t_utc': {'$gt': util.timestamp(start_date)}, 'properties.FeatureType': "beacon"}).sort([('properties.t_utc', DESCENDING)]).limit(1))[0]
             end_date = util.parse_date(last_feature['properties']['DateTime'])
             duration = end_date - start_date
-            expeditions[expedition] = {'StartDate': start_date, 'Days': duration.days}
+            expeditions[expedition] = {'StartDate': start_date, 'Days': duration.days, 'Name': strings.titlecase(" ".join(expedition.split("_")))}
     except Exception as e:
         return self.error(log.exc(e))
     total = returned = len(expeditions)
