13a0c7b6c87aa799a17907d08c3ff2c097b633e9
Ian Ardouin-Fumat <ian.ardouin@gmail.com>
2016-08-08 19:03:55 -0400

    added animation to background map

--- static/js/components/BackgroundMap.js
+++ static/js/components/BackgroundMap.js
@@ -1,24 +1,50 @@
 import React, { PropTypes } from 'react'
 import MapGL from 'react-map-gl'
+import I from 'immutable'
+import autobind from 'autobind-decorator'
 
-const BackgroundMap = () => {
+class BackgroundMap extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      foor: 'bar'
+    }
+  }
 
-  const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiaWFhYWFuIiwiYSI6ImNpbXF1ZW4xOTAwbnl3Ymx1Y2J6Mm5xOHYifQ.6wlNzSdcTlonLBH-xcmUdQ';
-  const MAPBOX_STYLE = 'mapbox://styles/iaaaan/cioxda1tz000cbnm13jtwhl8q';
+  @autobind
+  tick () {
+    const {animate} = this.props
+    if (animate) {
+      // console.log('aga')
+    }
+    requestAnimationFrame(this.tick)
+  }
 
-  return (
-    <div id="mapbox">
-      <MapGL 
-        mapStyle={MAPBOX_STYLE}
-        mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
-        width={window.innerWidth} height={window.innerHeight} latitude={37.7577} longitude={-122.4376}
-        zoom={8} onChangeViewport={(viewport) => {
-          const {latitude, longitude, zoom} = viewport;
-        }}
-      />
-    </div>
-  )
+  componentDidMount () {
+    this.tick()
+  }
+
+  render () {
+    const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiaWFhYWFuIiwiYSI6ImNpbXF1ZW4xOTAwbnl3Ymx1Y2J6Mm5xOHYifQ.6wlNzSdcTlonLBH-xcmUdQ'
+    const MAPBOX_STYLE = 'mapbox://styles/iaaaan/cioxda1tz000cbnm13jtwhl8q'
+
+    return (
+      <div id="mapbox">
+        <MapGL
+          mapStyle={MAPBOX_STYLE}
+          mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
+          width={window.innerWidth} height={window.innerHeight} latitude={37.7577} longitude={-122.4376}
+          zoom={8} onChangeViewport={(viewport) => {
+            const {latitude, longitude, zoom} = viewport
+          }}
+        />
+      </div>
+    )
+  }
 }
 
-BackgroundMap.propTypes = {}
+BackgroundMap.propTypes = {
+  animate: PropTypes.bool.isRequired
+}
+
 export default BackgroundMap
