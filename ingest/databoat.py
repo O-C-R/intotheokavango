73706a857480f3f898ca11a423a3fddbfefcceb4
brianhouse <brian.house@gmail.com>
2016-08-16 17:58:04 -0400

    reformat databoat protocol

--- ingest/databoat.py
+++ ingest/databoat.py
@@ -6,9 +6,7 @@ def parse(request):
     log.info("databoat.parse")
     data = ingest_json_body(request)
     try:
-        for key, reading in data.items():
-            t_local = strings.as_numeric(reading['t_local'])
-            reading = reading['data']        
+        for reading in data:
             log.debug(json.dumps(reading, indent=4, default=lambda x: str(x)))
             if 'gps_long' in reading and 'gps_lat' in reading:
                 reading['Longitude'] = reading['gps_long']
@@ -18,7 +16,6 @@ def parse(request):
             reading['FeatureType'] = "sensor"
             reading['FeatureSubType'] = "hybrid"
             reading['SensorName'] = "databoat"
-            reading['t_utc'] = util.delocalize_timestamp(t_local, tz=config['local_tz'])
             success, value = ingest_data("sensor", reading)
             if not success:
                 log.error("--> failed: %s" % value)            
@@ -28,15 +25,3 @@ def parse(request):
     return True
 
 
-"""
-{ t_local: '1429805254',
- data: 
-  { dissolved_oxygen: '9.79',
-    orp: '202.2',
-    salinity: '0.00',
-    ph: '3.747',
-    conductivity: '0',
-    'water temp': '0' },
- t_utc: 1429805257 }
-
-"""
