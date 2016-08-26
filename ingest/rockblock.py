61a1d62cfb177da0b7c6b5b592b0f10f4addb181
brianhouse <brian.house@gmail.com>
2016-08-26 09:52:33 -0400

    adding uptime to sensor ingest

--- ingest/rockblock.py
+++ ingest/rockblock.py
@@ -14,7 +14,7 @@ def parse(request):
 
     label_sets = [  ['t_utc', 'Station', 'Temp', 'Humidity', 'Pressure', 'WindSpeed2m', 'WindDir2m', 'WindGust10m', 'WindGustDir1m', 'DailyRain'],
                     ['t_utc', 'Station', 'Temp', 'Humidity', 'Pressure', 'WindSpeed2m', 'WindDir2m', 'WindGust10m', 'WindGustDir1m', 'DailyRain', 'Battery'],
-                    ['t_utc', 'Station', 'Latitude', 'Longitude', 'Altitude'],
+                    ['t_utc', 'Station', 'Latitude', 'Longitude', 'Altitude', 'Uptime'],
                     ['t_utc', 'Station', 'Battery', 'ORP', 'pH', 'DO', 'Ec', 'Tds', 'Sal', 'Sg', '0', '0', '0', 'WaterT']
                     ]
     values = data.split(",")
