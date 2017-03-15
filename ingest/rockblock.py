2dc214e38b11db749675d3d81c32939cc7721898
brianhouse <brian.house@gmail.com>
2017-03-15 16:27:30 -0400

    rockblock update to new format

--- ingest/rockblock.py
+++ ingest/rockblock.py
@@ -12,25 +12,24 @@ def parse(request):
         log.error(log.exc(e))
         return None, "Parsing error"
 
-    label_sets = [  ['t_utc', 'Station', 'Temp', 'Humidity', 'Pressure', 'WindSpeed2m', 'WindDir2m', 'WindGust10m', 'WindGustDir1m', 'DailyRain'],
-                    ['t_utc', 'Station', 'Temp', 'Humidity', 'Pressure', 'WindSpeed2m', 'WindDir2m', 'WindGust10m', 'WindGustDir1m', 'DailyRain', 'Battery'],
-                    ['t_utc', 'Station', 'Latitude', 'Longitude', 'Altitude', 'Uptime'],
-                    ['t_utc', 'Station', 'Battery', 'ORP', 'pH', 'DO', 'Ec', 'Tds', 'Sal', 'Sg', '0', '0', '0', 'WaterT']
-                    ]
     values = data.split(",")
-    labels = None
-    for label_set in label_sets:
-        if len(values) == len(label_set):
-            labels = label_set
-            break
-    if labels is None:
-        log.error("Message type not recognized, length not matched")
+    if value[0] == 'ST':
+        labels = 'VBat','Charge','HasSd','HasGps','BatSleep','DeepSleep','TxFailures','TxSkipped','WeatherReceived','AtlasReceived','SonarReceived','DeadFor','AvgTxTime'
+    elif value[0] == 'WE':
+        labels = 'VBat','Charge','Temp','Hum','Press','WindSpeed2','WindDir2','WindGust10','WindGustDir10','DailyRain'
+    elif value[0] == 'AT':
+        labels = 'Kind','VBat','Charge','Temp','Hum','Press','WindSpeed2','WindDir2','WindGust10','WindGustDir10','DailyRain'
+    elif value[0] == 'LO':        
+        labels = 'VBat','Charge','Lat','Lon','Alt','Uptime'
+    else:
+        log.error("Message type not recognized")
         log.debug(values)
-        return None, "Message type not recognized, length not matched"
+        return None, "Message type not recognized"
+
+    values = values[1:]
+
     feature = {'FeatureType': "sensor", 'Delivery': "rockblock"}
     for l, label in enumerate(labels):
-        if label == '0':
-            continue
         feature[label] = strings.as_numeric(values[l])
     log.debug(feature)
 
