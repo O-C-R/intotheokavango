8976cecc39d641d84c5579fc520ea7b26072c703
brianhouse <brian.house@gmail.com>
2017-03-15 21:58:56 -0400

    updated rockblock ingest

--- ingest/rockblock.py
+++ ingest/rockblock.py
@@ -13,20 +13,20 @@ def parse(request):
         return None, "Parsing error"
 
     values = data.split(",")
-    if value[0] == 'ST':
-        labels = 'VBat','Charge','HasSd','HasGps','BatSleep','DeepSleep','TxFailures','TxSkipped','WeatherReceived','AtlasReceived','SonarReceived','DeadFor','AvgTxTime'
-    elif value[0] == 'WE':
-        labels = 'VBat','Charge','Temp','Hum','Press','WindSpeed2','WindDir2','WindGust10','WindGustDir10','DailyRain'
-    elif value[0] == 'AT':
-        labels = 'Kind','VBat','Charge','Temp','Hum','Press','WindSpeed2','WindDir2','WindGust10','WindGustDir10','DailyRain'
-    elif value[0] == 'LO':        
-        labels = 'VBat','Charge','Lat','Lon','Alt','Uptime'
+    if value[2] == 'ST':
+        labels = 't_utc','Station','VBat','Charge','HasSd','HasGps','BatSleep','DeepSleep','TxFailures','TxSkipped','WeatherReceived','AtlasReceived','SonarReceived','DeadFor','AvgTxTime'
+    elif value[2] == 'WE':
+        labels = 't_utc','Station','VBat','Charge','Temp','Hum','Press','WindSpeed2','WindDir2','WindGust10','WindGustDir10','DailyRain'
+    elif value[2] == 'AT':
+        labels = 't_utc','Station','VBat','Charge','Temp','Hum','Press','WindSpeed2','WindDir2','WindGust10','WindGustDir10','DailyRain'
+    elif value[2] == 'LO':        
+        labels = 't_utc','Station','VBat','Charge','Latitude','Longitude','Altitude','Uptime'
     else:
         log.error("Message type not recognized")
         log.debug(values)
         return None, "Message type not recognized"
 
-    values = values[1:]
+    del values[2]
 
     feature = {'FeatureType': "sensor", 'Delivery': "rockblock"}
     for l, label in enumerate(labels):
