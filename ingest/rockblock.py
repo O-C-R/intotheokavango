3d1e2bb6586ce37d72fd86153ae1f1fcf439fdbe
brianhouse <brian.house@gmail.com>
2017-03-29 10:43:00 -0400

    added cascading parsing to rockblock

--- ingest/rockblock.py
+++ ingest/rockblock.py
@@ -35,6 +35,32 @@ def parse(request):
         return feature
 
     except Exception as e:
+        log.warning(log.exc(e))
+        log.info("Trying previous schema...")
+        
+    try:
+        label_sets = [  ['t_utc', 'Station', 'Temp', 'Humidity', 'Pressure', 'WindSpeed2m', 'WindDir2m', 'WindGust10m', 'WindGustDir1m', 'DailyRain'],
+                        ['t_utc', 'Station', 'Temp', 'Humidity', 'Pressure', 'WindSpeed2m', 'WindDir2m', 'WindGust10m', 'WindGustDir1m', 'DailyRain', 'Battery'],
+                        ['t_utc', 'Station', 'Latitude', 'Longitude', 'Altitude', 'Uptime'],
+                        ['t_utc', 'Station', 'Battery', 'ORP', 'pH', 'DO', 'Ec', 'Tds', 'Sal', 'Sg', '0', '0', '0', 'WaterT']
+                        ]
+        values = data.split(",")
+        labels = None
+        for label_set in label_sets:
+            if len(values) == len(label_set):
+                labels = label_set
+                break
+        if labels is None:
+            log.error("Message type not recognized, length not matched")
+            log.debug(values)
+            return None, "Message type not recognized, length not matched"
+        feature = {'FeatureType': "sensor", 'Delivery': "rockblock"}
+        for l, label in enumerate(labels):
+            if label == '0':
+                continue
+            feature[label] = strings.as_numeric(values[l])
+        log.debug(feature)
+        return feature
+    except Exception as e:
         log.error(log.exc(e))
-        log.error(data)
-        return None, "Parsing error"
+        return None, "Could not parse"
