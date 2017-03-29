9895c705b5135b49d7a5ce372bea5023ddb64329
brianhouse <brian.house@gmail.com>
2017-03-28 22:18:42 -0400

    debugging rockblock

--- ingest/rockblock.py
+++ ingest/rockblock.py
@@ -12,25 +12,31 @@ def parse(request):
         log.error(log.exc(e))
         return None, "Parsing error"
 
-    values = data.split(",")
-    if value[2] == 'ST':
-        labels = 't_utc','Station','VBat','Charge','HasSd','HasGps','BatSleep','DeepSleep','TxFailures','TxSkipped','WeatherReceived','AtlasReceived','SonarReceived','DeadFor','AvgTxTime'
-    elif value[2] == 'WE':
-        labels = 't_utc','Station','VBat','Charge','Temp','Hum','Press','WindSpeed2','WindDir2','WindGust10','WindGustDir10','DailyRain'
-    elif value[2] == 'AT':
-        labels = 't_utc','Station','VBat','Charge','Temp','Hum','Press','WindSpeed2','WindDir2','WindGust10','WindGustDir10','DailyRain'
-    elif value[2] == 'LO':        
-        labels = 't_utc','Station','VBat','Charge','Latitude','Longitude','Altitude','Uptime'
-    else:
-        log.error("Message type not recognized")
-        log.debug(values)
-        return None, "Message type not recognized"
+    try:
+        values = data.split(",")
+        if value[2] == 'ST':
+            labels = 't_utc','Station','VBat','Charge','HasSd','HasGps','BatSleep','DeepSleep','TxFailures','TxSkipped','WeatherReceived','AtlasReceived','SonarReceived','DeadFor','AvgTxTime'
+        elif value[2] == 'WE':
+            labels = 't_utc','Station','VBat','Charge','Temp','Hum','Press','WindSpeed2','WindDir2','WindGust10','WindGustDir10','DailyRain'
+        elif value[2] == 'AT':
+            labels = 't_utc','Station','VBat','Charge','Temp','Hum','Press','WindSpeed2','WindDir2','WindGust10','WindGustDir10','DailyRain'
+        elif value[2] == 'LO':        
+            labels = 't_utc','Station','VBat','Charge','Latitude','Longitude','Altitude','Uptime'
+        else:
+            log.error("Message type not recognized")
+            log.debug(values)
+            return None, "Message type not recognized"
 
-    del values[2]
+        del values[2]
 
-    feature = {'FeatureType': "sensor", 'Delivery': "rockblock"}
-    for l, label in enumerate(labels):
-        feature[label] = strings.as_numeric(values[l])
-    log.debug(feature)
+        feature = {'FeatureType': "sensor", 'Delivery': "rockblock"}
+        for l, label in enumerate(labels):
+            feature[label] = strings.as_numeric(values[l])
+        log.debug(feature)
 
-    return feature
+        return feature
+
+    except Exception as e:
+        log.error(log.exc(e))
+        log.error(data)
+        return None, "Parsing error"
