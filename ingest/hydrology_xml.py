2e424953d90f500f3ece25767e78f71eee7e61e5
brianhouse <brian.house@gmail.com>
2017-05-05 21:58:48 -0400

    adding hydrology

---
+++ ingest/hydrology_xml.py
@@ -0,0 +1,36 @@
+import json, xmltodict, os, base64
+from ingest import ingest_json_body, save_files, process_image, ingest_data, ingest_plain_body
+from housepy import config, log, util, strings
+from ingest.sighting import get_taxonomy
+
+def parse(request):
+    log.info("hydrology.parse")
+
+    try:
+        content = ingest_plain_body(request)
+        data = xmltodict.parse(content)
+    except Exception as e:
+        log.error(log.exc(e))
+        return None, "Parsing error"
+
+    try:
+        log.info("--> parsing XML")
+        data = data['instance']
+        log.debug(json.dumps(data, indent=4, default=lambda x: str(x)))
+
+        feature = {'FeatureType': "hydrology", 'Delivery': "devicemagic"}
+        feature['Member'] = data['@dm:submitting_user'].split(' ')[0]
+        dt = util.parse_date(data['@writeTime'])
+        data = data['inputs']
+
+        feature['t_utc'] = util.timestamp(dt)
+
+        # purge blanks
+        feature = {key: value for (key, value) in feature.items() if type(value) != str or len(value.strip())}
+
+    except Exception as e:
+        log.error(log.exc(e))
+        return None, "Unexpected fields"
+
+    return feature
+
