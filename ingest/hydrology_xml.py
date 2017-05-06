b1accf71d1b426a7c5fc521dab36ce3215df8269
brianhouse <brian.house@gmail.com>
2017-05-05 23:03:53 -0400

    processing nested

--- ingest/hydrology_xml.py
+++ ingest/hydrology_xml.py
@@ -1,4 +1,5 @@
 import json, xmltodict, os, base64
+from collections import OrderedDict
 from ingest import ingest_json_body, save_files, process_image, ingest_data, ingest_plain_body
 from housepy import config, log, util, strings
 from ingest.sighting import get_taxonomy
@@ -16,21 +17,20 @@ def parse(request):
     try:
         log.info("--> parsing XML")
         data = data['instance']
-        log.debug(json.dumps(data, indent=4, default=lambda x: str(x)))
 
         feature = {'FeatureType': "hydrology", 'Delivery': "devicemagic"}
         feature.update(data['inputs'])
         for key, value in feature.items():
-            if type(value) == dict:
+            if type(value) == OrderedDict:
                 for k, v, in value.items():
                     value[k] = strings.as_numeric(v)
         feature['Member'] = data['@dm:submitting_user'].split(' ')[0]
         dt = util.parse_date(data['@writeTime'])
         feature['t_utc'] = util.timestamp(dt)
+        log.debug(json.dumps(data, indent=4, default=lambda x: str(x)))
 
     except Exception as e:
         log.error(log.exc(e))
         return None, "Unexpected fields"
 
     return feature
-
